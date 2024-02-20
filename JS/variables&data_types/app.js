e612c0954a9dc905883ee7da8d3e86677eb18728
Bilal raza <attari1235@gmail.com>
2024-02-20 18:56:56 +0500

    Javascript variables

---
+++ JS/variables&data_types/app.js
@@ -0,0 +1,53 @@
+
+// javascript ka bana banaya function he jo pop up open krdeta he
+
+// javascript case sensitive he , 
+
+// alert("Hello World")
+
+//Variables are container for storing data.
+
+// Data ki 8 types hen
+
+// 1. String -> jo single quote ya double quote mein ho. e.g "Hello" , 'Hello'
+// 2. Number -> number ho or single quote ya double quote mein na ho. e.g 23 , 42
+// 3. Boolean -> true ,false 
+// 4. Undefined ->  variable banaya ho lekin usko koi value na di ho
+// 5. Object
+// 6. Array
+// 7. null
+// 8. function
+
+// alert('Welcome')
+
+// var ka keyword lagaenge phr relevent naam likhenge phr = lagaenge iske baad value dedenge
+
+var firstName = "Bilal" //string
+var lastName = "Raza" //string
+var age = 27 //number
+var isMarried = true //boolean
+var emaanParKhatima // undefined
+
+alert(bilal)
+alert(firstName)
+
+var bilal = 'abc'
+
+
+//q1
+alert('dsajkdlas')
+
+//q2
+
+
+// var 1stname
+
+// var delete
+// var for
+// alert(emaanParKhatima)
+// variable ke naam rakhne ke kuch rules hen
+// 1.naam mein space na ho
+// 2.number se start na ho , number beech or end mein asakta he e.g 1stname
+// 3.underscore or $ ke elawa koi special charactor na ho
+// 4.variable ka naam  javascript ke keyword ke naam par na ho 
+// 5.variable ka naam camelcase mein ho
