4bb07b132185ced4f9c54d092bc5f491dcbaaee6
Bilal raza <attari1235@gmail.com>
2024-02-27 18:40:48 +0500

    Airthmatic operators done

---
+++ JS/AirthmaticOperators/app.js
@@ -0,0 +1,43 @@
+var num1 = 30
+var num2 = 10
+
+var addition = num1 + num2
+var subtraction = num1 - num2
+var multiplication = num1 * num2
+var divide = num1 / num2
+var modulas = 9 % 4
+
+var bodmas = subtraction + multiplication - addition / 2 + divide * modulas
+
+var bodmas1 = 5 * 10 + 3
+var bodmas2 = 5 * 10 + addition / 2
+var bodmas3 = 5 * divide + addition - subtraction
+var bodmas4 = subtraction - divide + addition - subtraction
+
+// console.log(addition)
+// console.log(subtraction)
+// console.log(multiplication)
+// console.log(divide)
+// console.log(modulas)
+// console.log(bodmas)
+// console.log(bodmas1)
+// console.log(bodmas2)
+// console.log(bodmas3)
+// console.log(bodmas4)
+
+// string pe airthmatic operators ko apply krna
+
+// string mein agr ham number plus (+) krenge to wo concatenate hojaega or number bhe string mein convert hojaega
+// or agr plus ke 
+// elawa koi or operator lagaenge to agr string number huwa to apply hojaega warna
+// NaN aajaega
+
+var number1 = '40'
+var number2 = 5
+
+console.log(number1 + number2)
+console.log(number1 * number2)
+console.log(number1 - number2)
+console.log(number1 / number2)
+
+console.log("ABCD" * 5)
