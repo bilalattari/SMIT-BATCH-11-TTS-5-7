5bb3d28d5a66f64d1c9fde9c0bc36334ccefad6f
Bilal raza <attari1235@gmail.com>
2024-03-02 18:59:22 +0500

    prompt if else learned

---
+++ JS/prompt&ifelse/app.js
@@ -0,0 +1,48 @@
+// var firstName = prompt('Enter your first name?')
+// var lastName = prompt('Enter your last name?')
+
+// console.log(firstName)
+// console.log(lastName)
+
+// var tableNumber = prompt('Table you want')
+
+// document.write(`<h1> Table of ${tableNumber}</h1>`)
+// document.write(`<li>${tableNumber} x 1 = ${tableNumber * 1}</li>`)
+// document.write(`<li>${tableNumber} x 2 = ${tableNumber * 2}</li>`)
+// document.write(`<li>${tableNumber} x 3 = ${tableNumber * 3}</li>`)
+
+//round bracket ke andar condition lagegi
+//curly brackets mein agr condition true huwi to jo hamen kaam krwana
+// hoga wo curly brackets mein krenge
+
+//compraision oprators
+
+// ==   donu chezen equal hen ya nahn
+// ===  donu chezen equal hen ya nahn plus donu ki data type
+// !=  not equal to
+// > greater then
+// >= greater then or equals gto
+// < less then
+// =< less then or equals to
+
+// if (10 === '10') {
+//   console.log('Condition true')
+// } else {
+//   console.log('Condition false hogye')
+// }
+
+// var num = prompt('Give number and check if is even or Odd')
+
+// if (num % 2 == 0) {
+//   document.write(`<h2>${num} is even.</h2>`)
+// } else {
+//   document.write(`<h2>${num} is odd. </h2>`)
+// }
+
+var age = prompt('User Age')
+
+if (age >=  18) {
+  document.write(`<h2>You are adult.</h2>`)
+} else {
+  document.write(`<h2>You are not adult. </h2>`)
+}
