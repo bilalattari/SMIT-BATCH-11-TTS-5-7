623b7884e81a8b6d81f623afcd75e88e6da222b8
Bilal raza <attari1235@gmail.com>
2024-03-19 16:52:55 +0500

    Learned For Loop

---
+++ JS/forloop/app.js
@@ -0,0 +1,42 @@
+//1.variable banate hen
+//2.condition
+//3.increment ya decrement
+
+//simple loop
+// for (var i = 1; i <= 10; i++) {
+//   console.log("Hello " + i);
+// }
+
+//print from 1 to 100
+// for (var i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+//print square from 1 to 10
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i * i);
+// }
+
+//print square msg from 1 to 10
+// for (var i = 1; i <= 10; i++) {
+//   console.log("Square of " + i + " is " + i * i);
+// }
+
+//print even numbers only
+// for (var i = 1; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+//print odd numbers only
+// for (var i = 1; i <= 20; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+//print table of 4 from 1 to 10
+for (var i = 1; i <= 10; i++) {
+  console.log(i * 4);
+}
