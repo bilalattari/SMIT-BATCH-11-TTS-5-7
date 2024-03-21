2b2fbbef4c1d6620955f442b617ef9551bcd6e15
Bilal raza <attari1235@gmail.com>
2024-03-21 16:59:34 +0500

    For loop excercise

--- JS/forloop/app.js
+++ JS/forloop/app.js
@@ -37,6 +37,45 @@
 // }
 
 //print table of 4 from 1 to 10
-for (var i = 1; i <= 10; i++) {
-  console.log(i * 4);
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i * 4);
+// }
+
+//
+// var num = 0;
+// 1 3 6 10 15 21 28 36 45
+// for (var i = 0; i < 10; i++) {
+//   console.log("num=>", num, "i=>", i);
+//   num = num + i;
+//   console.log(num);
+// }
+
+//9,8,7,6,5,4,3,2,1
+// for (var i = 0; i < 10; i++) {
+//   console.log(9 - i);
+// }
+
+//19, 28, 37, 46, 55, 64, 73, 82, 91;
+// for (var i = 1; i < 10; i++) {
+//   console.log(i, 10 - i);
+// }
+
+//
+var nums = [20, 3, 12, 34, 21, 25, 6, 2];
+var largestNum = 0;
+for (var i = 0; i < nums.length; i++) {
+  console.log(
+    "index->",
+    i,
+    "arr value=>",
+    nums[i],
+    "largest number=>",
+    largestNum,
+    "condition=>",
+    nums[i] > largestNum
+  );
+  if (nums[i] > largestNum) {
+    largestNum = nums[i];
+  }
 }
+console.log("Largest number is-> ", largestNum);
