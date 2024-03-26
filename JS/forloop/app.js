07cffba2bdfac1e8e1d2e56926a8a3367216a7f6
Bilal raza <attari1235@gmail.com>
2024-03-26 16:57:59 +0500

    ]]

--- JS/forloop/app.js
+++ JS/forloop/app.js
@@ -96,26 +96,62 @@ console.log("Smallest number is-> ", smallestNum);
 //   document.write("You loss");
 // }
 
-var karachiKeAlaqe = [
-  "Saddar",
-  "Shah Faisal",
-  "Gulshan",
-  "Korangi",
-  "Dha",
-  "Lyari",
-  "Maleer",
-  "Nazimabad",
-  "Hyderi",
-];
-var userElaqa = prompt("Aap kahan tashref rakhte hen");
-var deliveryAvailable = false;
-for (var i = 0; i < karachiKeAlaqe.length; i++) {
-  if (karachiKeAlaqe[i] === userElaqa) {
-    deliveryAvailable = true;
-    document.write("Delivery Available");
-    break;
+// var karachiKeAlaqe = [
+//   "Saddar",
+//   "Shah Faisal",
+//   "Gulshan",
+//   "Korangi",
+//   "Dha",
+//   "Lyari",
+//   "Maleer",
+//   "Nazimabad",
+//   "Hyderi",
+// ];
+// var userElaqa = prompt("Aap kahan tashref rakhte hen");
+// var deliveryAvailable = false;
+// for (var i = 0; i < karachiKeAlaqe.length; i++) {
+//   if (karachiKeAlaqe[i] === userElaqa) {
+//     deliveryAvailable = true;
+//     document.write("Delivery Available");
+//     break;
+//   }
+// }
+// if (!deliveryAvailable) {
+//   document.write("Delivery Not Available");
+// }
+
+//nested loop=>
+// var userPrizeBond = [234, 235, 236, 237, 238, 323, 21, 313, 434, 124, 360];
+// var prizeBondWinner = [
+//   112, 152, 190, 201, 220, 236, 238, 250, 285, 310, 315, 320, 341, 360, 355,
+//   380, 400,
+// ];
+// var userWon = [];
+// for (var i = 0; i < userPrizeBond.length; i++) {
+//   for (var j = 0; j < prizeBondWinner.length; j++) {
+// console.log(
+//   "i=>",
+//   userPrizeBond[i],
+//   "j=>",
+//   prizeBondWinner[j],
+//   userPrizeBond[i] === prizeBondWinner[j]
+// );
+//     if (userPrizeBond[i] === prizeBondWinner[j]) {
+//       userWon.push(userPrizeBond[i]);
+//     }
+//   }
+// }
+// console.log("Prize bond ye nikle hen=>", userWon);
+
+var students1 = ["Ahmed", "Abdul", "Bilal", "Raza", "Haris", "Shoab"];
+var students2 = ["Hasan", "Tabish", "Usman", "Abdul", "Ahsan", "Hamza", "Bilal",];
+
+var duplication = []
+for(var i = 0; i < students1.length; i++){
+  for(var j = 0; j < students2.length; j++){
+    if(students1[i] === students2[j]){
+      duplication.push(students1[i])
+    }
   }
 }
-if (!deliveryAvailable) {
-  document.write("Delivery Not Available");
-}
+console.log('These names are  in both list=>',duplication)
