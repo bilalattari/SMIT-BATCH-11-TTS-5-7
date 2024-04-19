9166cb51f7ddd7a88c0a604588afa78bd515655f
M Bilal <121584126+mbilal-09@users.noreply.github.com>
2024-04-19 17:15:12 +0500

    Learning DOM

--- JS/changing case/script.js
+++ JS/changing case/script.js
@@ -1,3 +1,4 @@
+
 // var username = "basit";
 
 // username.toUpperCase();
@@ -29,24 +30,24 @@
 // )
 // console.log(a.length);
 
-var a = "Hello my name is Bilal.";
+// var a = "Hello my name is Bilal.";
 
-console.log(a[2]);
-console.log(a.slice(0, 5));
+// console.log(a[2]);
+// console.log(a.slice(0, 5));
 
-var username = "basit";
-var sentence =
-  "Send document on my email attari1235@gmail.com.After going through document I will be sending queries!.";
+// var username = "basit";
+// var sentence =
+//   "Send document on my email attari1235@gmail.com.After going through document I will be sending queries!.";
 
-console.log(username[0].toUpperCase() + username.slice(1));
+// console.log(username[0].toUpperCase() + username.slice(1));
 
-console.log(sentence.indexOf("document"));
+// console.log(sentence.indexOf("document"));
 
-console.log(sentence.lastIndexOf("document"));
+// console.log(sentence.lastIndexOf("document"));
 
-//charAt
+// //charAt
 
-console.log(sentence.charAt(3));
+// console.log(sentence.charAt(3));
 
 // for (var i = 0; i < sentence.length; i++) {
 //   if (sentence.charAt(i) === "!") {
@@ -57,11 +58,104 @@ console.log(sentence.charAt(3));
 // }
 
 //replace
-var txt = "Bilal got first position in class.";
-txt = txt.replace("first", "1st");
-console.log(txt);
+// var txt = "Bilal got first position in class.";
+// txt = txt.replace("first", "1st");
+// console.log(txt);
 
-//trim
-var email = "  attari1235@gmail.com   ";
-console.log(email.length);
-console.log("Trim=>", email.trim().length);
+// //trim
+// var email = "  attari1235@gmail.com   ";
+// console.log(email.length);
+// console.log("Trim=>", email.trim().length);
+
+// var sentence = "Javascript is Wonderful Programing Language";
+
+// var vowelCount = 0;
+// var constCount = 0;
+
+// for (let i = 0; i < sentence.length; i++) {
+//   if (sentence[i].toLowerCase() === "a" || sentence[i].toLowerCase() === "e" || sentence[i].toLowerCase() === "i" || sentence[i].toLowerCase() === "o" || sentence[i].toLowerCase() === "u") {
+//     vowelCount++;
+//   } else if (sentence[i] != " ") {
+//     constCount++;
+//   }
+// }
+
+// var vowels = ["a", "e", "i", "o", "u"];
+
+// for (let i = 0; i < sentence.length; i++) {
+//   var vowelFound = false;
+
+//   for (var j = 0; j < vowels.length; j++) {
+//     if (sentence[i].toLowerCase() === vowels[j]) {
+//       vowelFound = true;
+//       break;
+//     }
+//   }
+//   if (vowelFound) vowelCount++;
+//   if (!vowelFound && sentence[i] !== " ") constCount++;
+// }
+
+// console.log("vowelCount-> ", vowelCount);
+// console.log("constCount-> ", constCount);
+
+// var sentence = "Javascript is Wonderful Programing Language";
+
+//Q1.Count the number of letter a used in sentence.
+
+// var count = 0;
+// for (var i = 0; i < sentence.length; i++) {
+//   if (sentence.charAt(i).toLowerCase() === "a") count++;
+// }
+
+// console.log("Number A's in this sentence are", count);
+
+//Q2.Capitilize first letter of each word.
+
+// var userName = "bilal raza aaa aa aaa aa";
+// var arr = userName.split(" ");
+// console.log(userName);
+// console.log(arr);
+
+// for (var i = 0; i < arr.length; i++) {
+//   var word = arr[i];
+//   arr[i] = word[0].toUpperCase() + word.slice(1);
+// }
+
+// console.log(arr);
+// var updated = arr.join(" ");
+// console.log(updated);
+
+//Q3.Password must include 1 capital letter , 1 number , min length should be 8.
+
+var UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var SPECIAL = "!_@$";
+ 
+var password = prompt("Enter password min length 8 , 1 Uppercase , 1 Number");
+
+console.log(password);
+
+// var userName = "bilal raza aaa aa aaa aa";
+// var arr = userName.split(" ");
+// console.log(userName);
+// console.log(arr);
+
+// for (var i = 0; i < arr.length; i++) {
+//   var word = arr[i];
+//   arr[i] = word[0].toUpperCase() + word.slice(1);
+// }
+
+// console.log(arr);
+// var updated = arr.join(" ");
+// console.log(updated);
+
+//document object modal
+
+var car = {
+  name: "Civic",
+  modal: 2020,
+};
+
+var h1 = document.getElementById("heading");
+console.log(h1);
+
+h1.innerText = "We are learning DOM";
