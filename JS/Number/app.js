90f0e54be918efc9055d496a6a88c980b81d0478
mbilal-09 <bilakhanshakir@gmail.com>
2024-04-27 19:00:23 +0500

    guess hthe number

--- JS/Number/app.js
+++ JS/Number/app.js
@@ -31,41 +31,75 @@ function generateOTPNumber() {
   heading.innerText = random;
 }
 
-// Generate Dice Number
-var dice = document.getElementById("dice");
-var win_loss = document.getElementById("win_loss");
-var dice_input = document.getElementById("dice_input");
+// var string = "ABCSD123asSALNFzALIsadWQnkdaslnd";
+// var password_h1 = document.getElementById("password");
 
-function generateDiceNumber() {
-  var userValue = dice_input.value;
-  if (userValue < 7) {
-    var diceNumber = Math.ceil(Math.random() * 6);
-    dice.innerText = diceNumber;
-    if (userValue == diceNumber) {
-      win_loss.innerText = "You win";
-      win_loss.style.color = "Green";
-    } else {
-      win_loss.innerText = "You loss, Try Again";
-      win_loss.style.color = "red";
-    }
+// var password = "";
+
+// function generatePassword() {
+//   password = "";
+//   for (var i = 0; i < 10; i++) {
+//     var randomNumber = Math.round(Math.random() * string.length);
+//     console.log("randomNumber=>", randomNumber, string.charAt(randomNumber));
+//     password = password + string.charAt(randomNumber);
+//     console.log(password);
+//   }
+//   password_h1.innerText = password;
+// }
+
+var user_number = document.getElementById("user_number");
+var random_number = document.getElementById("random_number");
+var result = document.getElementById("result");
+var info_box_result = document.getElementById("info_box_result");
+var win_html = document.getElementById("win");
+var loss_html = document.getElementById("loss");
+var total = document.getElementById("total");
+var msg = document.getElementById("msg");
+
+var count = 10;
+var win = 0;
+var loss = 0;
+
+function tryMyLuck(userNumber) {
+  // alert(userNumber);
+  var randomNumber = Math.random() * 6;
+  var ceil = Math.ceil(randomNumber);
+  console.log(ceil);
+  user_number.innerText = userNumber;
+  random_number.innerText = ceil;
+
+  // total count mein se aik minus krdo
+  total.innerText = --count;
+  
+  if (ceil === userNumber) {
+    console.log("user wins");
+    result.innerText = "You won";
+    info_box_result.style.backgroundColor = "Green";
+    result.style.color = "White";
+    // total count mein se aik minus krdo
+    win_html.innerText = ++win;
   } else {
-    win_loss.innerText = "Number sahi daalo!";
-    win_loss.style.color = "orange";
+    result.innerText = "You Loss";
+    info_box_result.style.backgroundColor = "red";
+    result.style.color = "White";
+    loss_html.innerText = ++loss;
   }
+
+  checkResult();
 }
 
-var string = "ABCSD123asSALNFzALIsadWQnkdaslnd";
-var password_h1 = document.getElementById("password");
-
-var password = "";
-
-function generatePassword() {
-  password = "";
-  for (var i = 0; i < 10; i++) {
-    var randomNumber = Math.round(Math.random() * string.length);
-    console.log("randomNumber=>", randomNumber, string.charAt(randomNumber));
-    password = password + string.charAt(randomNumber);
-    console.log(password);
+function checkResult() {
+  if (count == 0) {
+    if (win >= 3) {
+      msg.innerText = "Congrats! You won";
+    } else {
+      msg.innerText = "Sorry! You loss try again";
+    }
+    count = 10;
+    win = 0;
+    loss = 0;
+    total.innerText = count;
+    win_html.innerText = loss;
+    loss_html.innerText = win;
   }
-  password_h1.innerText = password;
 }
