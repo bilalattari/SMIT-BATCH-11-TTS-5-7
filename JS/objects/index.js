4dd2ed14ffd7fef5c7ca9710855dacf14f54e71a
Bilal raza <attari1235@gmail.com>
2024-05-31 02:07:51 +0500

    constructor function

--- JS/objects/index.js
+++ JS/objects/index.js
@@ -20,90 +20,65 @@ var obj = {
 // document.getElementById("fathername").innerText = allUsers[1].fatherName;
 // document.getElementById("age").innerText = allUsers[1].getAge();
 
+function HelloMsg(msg, name, attendance) {
+  // console.log("this==>", this);
+  this.message = msg;
+  this.name = name;
+  this.isStudent = true;
+  this.attendance = attendance;
+  this.totalClasses = 50;
+  this.isPunctual = function () {
+    var perc = this.attendance / this.totalClasses;
+    if (perc >= 0.9) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+}
+
+var student1 = new HelloMsg("Welcome", "Bilal", 45);
+var student2 = new HelloMsg("Khush Amdeed", "Ahmed", 40);
+var student3 = new HelloMsg("Marhaba", "Abdullah", 30);
+var student4 = new HelloMsg("Tashreef layye", "Usman", 22);
+var student5 = new HelloMsg("Pakher Ragal", "Nawaz", 50);
+var student6 = new HelloMsg("Ji Aayano", "Imran", 48);
+
 var list = document.getElementById("list");
-var fruits = [
-  {
-    name: "Mango",
-    title: "Phalon ka badshah",
-    types: ["Chounsa", "Sindhri", "Langra", "Anwaratol", "Almaas"],
-    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
-    image:
-      "https://images.unsplash.com/photo-1591073113125-e46713c829ed?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8TWFuZ298ZW58MHx8MHx8fDA%3D",
-  },
-  {
-    name: "Watermelon",
-    title: "Garmi ka dushman",
-    types: ["Red Melon", "Green Melon"],
-    image:
-      "https://images.unsplash.com/photo-1621583441131-c8c190794970?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0ZXIlMjBtZWxvbnxlbnwwfHwwfHx8MA%3D%3D",
-  },
-  {
-    name: "Peach",
-    title: "Juicy Peach",
-    types: ["Swat Wala", "Quetta Wala"],
-    image:
-      "https://images.unsplash.com/photo-1629828874514-c1e5103f2150?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGVhY2h8ZW58MHx8MHx8fDA%3D",
-  },
-  {
-    name: "Cherry",
-    title: "Khoon banane wali",
-    types: ["Black Cherry", "Red Cherry"],
-    image:
-      "https://plus.unsplash.com/premium_photo-1688671921089-628b61dae149?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlcnJ5fGVufDB8fDB8fHww",
-  },
-];
+var allStudents = [student1, student2, student3, student4, student5, student6];
 
-fruits.forEach(function (data, ind) {
-  var ele = `<div class="lg:w-1/3  sm:w-1/2 p-4">
-  <div class="flex relative h-[250px]">
-    <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="${
-      data.image
-    }">
-    <div id = ${ind} class="px-8 py-10  relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-      <h2 class="tracking-widest text-xl title-font font-medium text-indigo-500 mb-1">${
-        data.name
-      }</h2>
-      <h1 class="title-font text-2xl font-medium text-gray-900 mb-3">${
-        data.title
-      }</h1>
-      <p class="leading-relaxed">${data.types.join(" , ")}</p>
-      <p class="leading-relaxed">${data.desc} </p>
-      <button onclick="updateDesc(this)">update</button>
-    </div>
-  </div>
-</div>`;
-  list.innerHTML += ele;
-});
+console.log("allStudents==>", allStudents);
 
-function updateDesc(ele) {
-  console.log("ele=>", ele.previousElementSibling);
-  console.log("ele parentr=>", ele.parentElement);
+// for (var i = 0; i < allStudents.length; i++) {
+//   // console.log("i=>", i, "data=>", allStudents[i]);
+//   // console.log(`${allStudents[i].message} ${allStudents[i].name}`);
+// }
 
-  var index = ele.parentElement.id;
-  fruits[index].desc = prompt("Desc");
-  console.log("fruits-->", fruits);
+function renderData() {
   list.innerHTML = "";
-  fruits.forEach(function (data, ind) {
-    var ele = `<div class="lg:w-1/3  sm:w-1/2 p-4">
-    <div class="flex relative h-[250px]">
-      <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="${
-        data.image
-      }">
-      <div id = ${ind} class="px-8 py-10  relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-        <h2 class="tracking-widest text-xl title-font font-medium text-indigo-500 mb-1">${
-          data.name
-        }</h2>
-        <h1 class="title-font text-2xl font-medium text-gray-900 mb-3">${
-          data.title
-        }</h1>
-        <p class="leading-relaxed">${data.types.join(" , ")}</p>
-        <p class="leading-relaxed">${data.desc} </p>
-        <button onclick="updateDesc(this)">update</button>
-      </div>
-    </div>
-  </div>`;
-    list.innerHTML += ele;
+  allStudents.forEach(function (data, index) {
+    list.innerHTML += `<div id= ${index} class = 'flex items-center'><h1 class ='text-2xl my-4'> ${data.message} ${data.name}</h1> 
+    <button class= 'ms-4 px-3 h-[30px] text-white outline-primary bg-red-500' onclick="deleteObj(this)">Delete</button>
+    <button class= 'ms-4 px-3 h-[30px] text-white outline-primary bg-blue-500' onclick="editObj(this)">Edit</button> </div>`;
   });
-  //   var desc = prompt("Desc");
-  //   ele.previousElementSibling.innerText = desc;
+}
+renderData();
+
+function deleteObj(ele) {
+  // console.log(ele.parentElement.id);
+  // console.log(allStudents[ele.parentElement.id]);
+  allStudents.splice(ele.parentElement.id, 1);
+  console.log("allStudents===>", allStudents);
+  renderData();
+}
+
+function editObj(ele) {
+  // console.log(ele.parentElement.id);
+  console.log(allStudents[ele.parentElement.id]);
+  var obj = allStudents[ele.parentElement.id];
+  obj.name = prompt('Update your name' ,obj.name );
+  renderData()
+
+  // allStudents.splice(ele.parentElement.id, 1);
+  // console.log("allStudents===>", allStudents);
 }
