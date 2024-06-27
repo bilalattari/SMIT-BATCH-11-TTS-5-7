5e14be4593e8ea45fcecbe3d6d53a284743991f8
Bilal raza <attari1235@gmail.com>
2024-06-27 17:25:41 +0500

    small example

--- JS/Diff-ForEach-Map/app.js
+++ JS/Diff-ForEach-Map/app.js
@@ -7,3 +7,17 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // console.log("square=>", square);
 // console.log("numbers=>", numbers);
+
+var a = "A";
+var b = "B";
+var c = "C";
+
+var p = new Promise((resolve) => resolve());
+
+p.then(() => console.log(c));
+
+setTimeout(() => {
+  console.log(a);
+}, 0);
+
+console.log(b);
